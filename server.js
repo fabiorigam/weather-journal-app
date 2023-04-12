197fcee2177323727a0214458112a7500504027d
fabiorigam <fabio.rigamonti@icloud.com>
2023-04-12 15:39:28 +0200

    Base structure

---
+++ server.js
@@ -0,0 +1,48 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+};
+
+//GET route that returns the projectData object
+app.get('/all', sendData)
+
+function sendData (request, response) {
+    response.send(projectData)
+}
+
+// POST route
+app.post('/addWeatherData', addData)
+
+function addData(request, response) {
+    projectData.temperature = request.body.temperature;
+    projectData.date = request.body.date;
+    projectData.user_response = request.body.user_response;
+    response.end();
+    console.log(projectData)
+}
